b649215aa0eee1a9c9c16eb095fce5197b04fc92
mahak shivhare <shivharemahak272@gmail.com>
2026-03-18 16:24:46 +0530

    Add files via upload

---
+++ continue_divisible.java
@@ -0,0 +1,11 @@
+//     ^
+public class continue_divisible {
+     public static void main(String[]args){
+        for(int i=18;i<=50;i++){
+            if(i%17==0){
+                break;
+            }
+            System.out.println(i);
+        }
+}
+}
