166703088ef0257ae3191cd42f94ff48fb859871
mahak shivhare <shivharemahak272@gmail.com>
2026-03-18 16:22:58 +0530

    Add files via upload

---
+++ areaofsquare.java
@@ -0,0 +1,9 @@
+     //area of square ^
+public class areaofsquare {
+    public static void main(String args[]){
+        int side=5;
+        int area;
+        area=side*side;
+        System.out.println(area);
+    }
+}
