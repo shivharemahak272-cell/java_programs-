3e362bd938c84207a45952cf8171388220974ffb
mahak shivhare <shivharemahak272@gmail.com>
2026-03-18 16:22:34 +0530

    Add files via upload

---
+++ areaofcircle.java
@@ -0,0 +1,11 @@
+   //area of circle ^
+public class areaofcircle {
+     public static void main(String[] args) {
+        float pi=22/7;
+        float r=5;
+        float areaofcircle;
+        areaofcircle=pi*r*r;
+        System.out.println(areaofcircle);
+
+    }   
+}
