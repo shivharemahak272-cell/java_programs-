17cf3728900faad3f88b7e843e8216eb1e438892
mahak shivhare <shivharemahak272@gmail.com>
2026-03-18 16:20:47 +0530

    Add files via upload

---
+++ add_array.java
@@ -0,0 +1,15 @@
+public class add_array {
+     public static void main(String[]args){
+        int sum=0;
+        int mark[]=new int[4];
+         mark[0]=10;
+        mark[1]=20;
+        mark[2]=30;
+        mark[3]=40;
+        for(int i=0;i<mark.length;i++){
+            sum=sum+i;
+        
+        System.out.println(sum);
+        }
+     }
+}
