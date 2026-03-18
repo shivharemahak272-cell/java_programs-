1abd72036a64d7b7c22c3c672d500542a5364ae6
mahak shivhare <shivharemahak272@gmail.com>
2026-03-18 16:22:14 +0530

    Add files via upload

---
+++ alzebra.java
@@ -0,0 +1,10 @@
+           //(a+b)**2 ^
+public class alzebra {
+     public static void main(String args[]){
+        int a=5;
+        int b=6;
+        int c;
+        c=(a*a)+(b*b)+2*a*b;
+        System.out.println(c);
+    }
+}
