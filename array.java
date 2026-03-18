134d61aa380932857b21b6bc37a3be0266aa404e
mahak shivhare <shivharemahak272@gmail.com>
2026-03-18 16:23:23 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,11 @@
+public class array {
+    public static void main(String[] args) {
+        int mark[]=new int[5];
+        mark[0]=12;
+        mark[1]=13;
+        mark[2]=14;
+        mark[3]=15;
+        mark[4]=16;
+        System.out.println(mark[3]);
+    }
+}
